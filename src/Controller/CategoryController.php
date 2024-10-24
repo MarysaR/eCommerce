0cebb59c024a32720e5863bee59d5e314009daad
neycha <regentmarysapro@gmail.com>
2024-10-24 10:17:13 +0200

    :feat: add bootstrap bundle, add Home, Category and ProductControllers files and twigs file

---
+++ src/Controller/CategoryController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class CategoryController extends AbstractController
+{
+  #[Route('/category/', name: 'categories')]
+  public function index(): Response
+  {
+    return $this->render('category/category.html.twig', [
+
+      'category' => 'Catégories',
+
+    ]);
+  }
+}
---
+++ src/Controller/ProductController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class ProductController extends AbstractController
+{
+  #[Route('/product/', name: 'products')]
+  public function index(): Response
+  {
+    return $this->render('product/product.html.twig', [
+
+      'product' => 'Produits',
+
+    ]);
+  }
+}
